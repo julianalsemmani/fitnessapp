ee591ab3f488d27c400ab16573c962dd51dd8023
ealangst <ealangst@hiof.no>
2022-09-29 16:33:38 +0200

    Cherry pick: Moved camera into ExerciseCameraFragment
    This is to make it easier to implement UI changes since
    camera code and ml-kit code was occupying MainActivity
    
    # Conflicts:
    #	app/src/main/java/com/groupfive/fitnessapp/MainActivity.kt

---
+++ app/src/main/java/com/groupfive/fitnessapp/ExerciseCameraFragment.kt
@@ -0,0 +1,129 @@
+package com.groupfive.fitnessapp
+
+import android.Manifest
+import android.os.Build
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.video.Recorder
+import androidx.camera.video.Recording
+import androidx.camera.video.VideoCapture
+import androidx.core.content.ContextCompat
+import com.google.mlkit.vision.pose.PoseDetection
+import com.google.mlkit.vision.pose.defaults.PoseDetectorOptions
+import com.groupfive.fitnessapp.databinding.FragmentExerciseCameraBinding
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+
+class ExerciseCameraFragment : Fragment() {
+
+    companion object {
+        private const val TAG = "ExerciseCameraFragment"
+    }
+
+//    val options = PoseDetectorOptions.Builder()
+//        .setDetectorMode(PoseDetectorOptions.STREAM_MODE)
+//        .build()
+//
+//    val poseDetector = PoseDetection.getClient(options)
+
+    private lateinit var binding: FragmentExerciseCameraBinding
+
+    private lateinit var cameraExecutor: ExecutorService
+
+    private var imageCapture: ImageCapture? = null
+
+    private var videoCapture: VideoCapture<Recorder>? = null
+    private var recording: Recording? = null
+
+
+    private val activityResultLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+            // Handle Permission granted/rejected
+            if (isGranted) {
+                // Permission is granted
+                startCamera()
+            } else {
+                // Permission is denied
+                Toast.makeText(context,
+                    "Permissions not granted by the user.",
+                    Toast.LENGTH_SHORT).show()
+            }
+        }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        Log.e(TAG, "CREATED FRAGMENT!!!!!!!!")
+
+        // Request camera permission
+        activityResultLauncher.launch(Manifest.permission.CAMERA)
+        cameraExecutor = Executors.newSingleThreadExecutor()
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        binding = FragmentExerciseCameraBinding.inflate(layoutInflater)
+
+        return binding.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        cameraExecutor.shutdown()
+    }
+
+    private fun startCamera() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(requireContext())
+
+        cameraProviderFuture.addListener({
+            // Used to bind the lifecycle of cameras to the lifecycle owner
+            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+
+            // Preview
+            val preview = Preview.Builder()
+                .build()
+                .also {
+                    it.setSurfaceProvider(binding.viewFinder.surfaceProvider)
+                }
+
+            // Select back camera as a default
+            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+
+            try {
+                // Unbind use cases before rebinding
+                cameraProvider.unbindAll()
+
+                // Bind use cases to camera
+                cameraProvider.bindToLifecycle(
+                    this, cameraSelector, preview)
+
+            } catch(exc: Exception) {
+                Log.e(TAG, "Use case binding failed", exc)
+            }
+
+        }, ContextCompat.getMainExecutor(requireContext()))
+    }
+}
+
+//@ExperimentalGetImage private class ImageAnalyzer : ImageAnalysis.Analyzer {
+//
+//    override fun analyze(imageProxy: ImageProxy) {
+//        val mediaImage = imageProxy.image
+//        if (mediaImage != null) {
+//            val image = InputImage.fromMediaImage(mediaImage, imageProxy.imageInfo.rotationDegrees)
+//        }
+//    }
+//}
