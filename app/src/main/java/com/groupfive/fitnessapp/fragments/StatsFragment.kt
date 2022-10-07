aec57bced4d3fea7852654f66fa2caa46ef29a61
Ibragim Yusupov <70488743+IbragimYusupov@users.noreply.github.com>
2022-10-07 15:22:46 +0200

    Fragment removed. Added new fragments.
    Removed BottomNavigationFragment as there was no need for it. Added new fragments for home, stats, notifications and profile.

--- app/src/main/java/com/groupfive/fitnessapp/fragments/StatsFragment.kt
+++ app/src/main/java/com/groupfive/fitnessapp/fragments/StatsFragment.kt
@@ -1,10 +1,11 @@
-package com.groupfive.fitnessapp
+package com.groupfive.fitnessapp.fragments
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.groupfive.fitnessapp.R
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -13,10 +14,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [BottomNavigationFragment.newInstance] factory method to
+ * Use the [StatsFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class BottomNavigationFragment : Fragment() {
+class StatsFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -34,7 +35,7 @@ class BottomNavigationFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_bottom_navigation, container, false)
+        return inflater.inflate(R.layout.fragment_stats, container, false)
     }
 
     companion object {
@@ -44,12 +45,12 @@ class BottomNavigationFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment BottomNavigationFragment.
+         * @return A new instance of fragment StatsFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            BottomNavigationFragment().apply {
+            StatsFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
