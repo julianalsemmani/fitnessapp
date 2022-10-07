bc7eb53c3aa06701ab762eb3231a08ab6389d0ea
ealangst <ealangst@hiof.no>
2022-10-07 12:41:21 +0200

    Detect arm raising and count reps

--- app/src/main/java/com/groupfive/fitnessapp/ExerciseCameraFragment.kt
+++ app/src/main/java/com/groupfive/fitnessapp/ExerciseCameraFragment.kt
@@ -21,12 +21,16 @@ import androidx.camera.video.Recording
 import androidx.camera.video.VideoCapture
 import androidx.core.content.ContextCompat
 import com.google.mlkit.vision.common.InputImage
+import com.google.mlkit.vision.pose.Pose
 import com.google.mlkit.vision.pose.PoseDetection
 import com.google.mlkit.vision.pose.PoseDetector
+import com.google.mlkit.vision.pose.PoseLandmark
 import com.google.mlkit.vision.pose.defaults.PoseDetectorOptions
 import com.groupfive.fitnessapp.databinding.FragmentExerciseCameraBinding
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
+import kotlin.math.abs
+import kotlin.math.atan2
 
 @ExperimentalGetImage
 class ExerciseCameraFragment : Fragment() {
@@ -127,14 +131,59 @@ class ExerciseCameraFragment : Fragment() {
     }
 
     private class ImageAnalyzer(val poseDetector: PoseDetector) : ImageAnalysis.Analyzer {
+        fun getAngle(firstPoint: PoseLandmark, midPoint: PoseLandmark, lastPoint: PoseLandmark): Double {
+            var result = Math.toDegrees(
+                (atan2(lastPoint.position.y - midPoint.position.y,
+                    lastPoint.position.x - midPoint.position.x)
+                        - atan2(firstPoint.position.y - midPoint.position.y,
+                    firstPoint.position.x - midPoint.position.x)).toDouble()
+            )
+            result = abs(result) // Angle should never be negative
+            if (result > 180) {
+                result = 360.0 - result // Always get the acute representation of the angle
+            }
+            return result
+        }
+
+        fun getAngle(pose: Pose, firstPoint: Int, midPoint: Int, lastPoint: Int) : Double {
+            return getAngle(
+                pose.getPoseLandmark(firstPoint)!!,
+                pose.getPoseLandmark(midPoint)!!,
+                pose.getPoseLandmark(lastPoint)!!)
+        }
+
+        var armUp = false
+        var reps = 0
+
         override fun analyze(imageProxy: ImageProxy) {
             val mediaImage = imageProxy.image
             if (mediaImage != null) {
                 val image = InputImage.fromMediaImage(mediaImage, imageProxy.imageInfo.rotationDegrees)
 
                 poseDetector.process(image)
-                    .addOnSuccessListener { results ->
-                        Log.w(TAG, results.allPoseLandmarks.map{ it.position3D.toString() }.toString())
+                    .addOnSuccessListener { pose ->
+                        if(pose.allPoseLandmarks.isNotEmpty()) {
+//                            Log.w(TAG, pose.allPoseLandmarks.map{ it.position3D.toString() }.toString())
+
+                            val rightHipAngle = getAngle(pose,
+                                PoseLandmark.RIGHT_WRIST,
+                                PoseLandmark.RIGHT_SHOULDER,
+                                PoseLandmark.RIGHT_HIP)
+
+                            if(armUp) {
+                                if(rightHipAngle > 120) {
+                                    armUp = false
+                                }
+                            } else {
+                                if(rightHipAngle < 30) {
+                                    armUp = true
+                                    reps++
+                                    Log.w(TAG, "Reps: $reps")
+                                }
+                            }
+
+//                            Log.w(TAG, "Right hip $rightHipAngle")
+                        }
                     }
                     .addOnFailureListener { exception ->
                         Log.e(TAG, "ml-kit failed to process image from camera: " + exception.message)
