6fe7cabc42671bdcb34438ae447f7e79bddd5d37
ealangst <ealangst@hiof.no>
2022-10-12 19:33:41 +0200

    Minor cleanup for exercise camera code

--- app/src/main/java/com/groupfive/fitnessapp/exercise/SquatExerciseDetector.kt
+++ app/src/main/java/com/groupfive/fitnessapp/exercise/SquatExerciseDetector.kt
@@ -6,14 +6,14 @@ import com.google.mlkit.vision.pose.PoseLandmark
 
 class SquatExerciseDetector : ExerciseDetector {
 
-    val downConstraints = ExercisePoseConstraints(
+    private val squatDownConstraints = ExercisePoseConstraints(
         ExerciseAngleConstraint(PoseLandmark.LEFT_ANKLE, PoseLandmark.LEFT_KNEE, PoseLandmark.LEFT_HIP,
             ComparisonType.SMALLER_THAN, 95.0),
         ExerciseAngleConstraint(PoseLandmark.RIGHT_ANKLE, PoseLandmark.RIGHT_KNEE, PoseLandmark.RIGHT_HIP,
             ComparisonType.SMALLER_THAN, 95.0)
     )
 
-    val upConstraints = ExercisePoseConstraints(
+    private val squatUpConstraints = ExercisePoseConstraints(
         ExerciseAngleConstraint(PoseLandmark.LEFT_ANKLE, PoseLandmark.LEFT_KNEE, PoseLandmark.LEFT_HIP,
             ComparisonType.GREATER_THAN, 160.0),
         ExerciseAngleConstraint(PoseLandmark.RIGHT_ANKLE, PoseLandmark.RIGHT_KNEE, PoseLandmark.RIGHT_HIP,
@@ -23,17 +23,13 @@ class SquatExerciseDetector : ExerciseDetector {
     var isSquatDown = true
 
     override fun detectRepetition(pose: Pose): Boolean {
-        Log.w(javaClass.name, ExerciseUtils.getAngle(pose,
-            PoseLandmark.LEFT_ANKLE,
-            PoseLandmark.LEFT_KNEE,
-            PoseLandmark.LEFT_HIP).toString())
         if(isSquatDown) {
-            if(downConstraints.evaluate(pose)) {
+            if(squatDownConstraints.checkPose(pose)) {
                 isSquatDown = false
                 Log.w(javaClass.name, "DOWN")
             }
         } else {
-            if(upConstraints.evaluate(pose)) {
+            if(squatUpConstraints.checkPose(pose)) {
                 isSquatDown = true
                 Log.w(javaClass.name, "UP")
                 return true
