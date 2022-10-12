8362062559de5ac2d575bd60a4873feb37bf28b5
Ibragim Yusupov <70488743+IbragimYusupov@users.noreply.github.com>
2022-10-12 20:51:00 +0200

    Removed unnecessary imports

--- app/src/main/java/com/groupfive/fitnessapp/MyApp.kt
+++ app/src/main/java/com/groupfive/fitnessapp/MyApp.kt
@@ -5,7 +5,6 @@ import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.content.Context
 import android.os.Build
-import androidx.core.app.NotificationManagerCompat
 
 class MyApp: Application() {
     override fun onCreate() {
