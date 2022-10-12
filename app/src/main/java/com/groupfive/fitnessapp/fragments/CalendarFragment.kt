7da2732044f0a035384d7cf67ba75fb68474c767
ealangst <ealangst@hiof.no>
2022-10-12 13:42:42 +0200

    CalendarFragment and DayViewContainer

---
+++ app/src/main/java/com/groupfive/fitnessapp/fragments/CalendarFragment.kt
@@ -0,0 +1,54 @@
+package com.groupfive.fitnessapp.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.groupfive.fitnessapp.R
+import com.groupfive.fitnessapp.calendar.DayViewContainer
+import com.groupfive.fitnessapp.databinding.FragmentCalendarBinding
+import com.kizitonwose.calendarview.model.CalendarDay
+import com.kizitonwose.calendarview.ui.DayBinder
+import java.time.YearMonth
+import java.time.temporal.WeekFields
+import java.util.*
+
+class CalendarFragment : Fragment() {
+
+    private lateinit var binding: FragmentCalendarBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentCalendarBinding.inflate(inflater)
+
+        binding.calendarView.dayBinder = object : DayBinder<DayViewContainer> {
+            // Called only when a new container is needed.
+            override fun create(view: View) = DayViewContainer(view)
+
+            // Called every time we need to reuse a container.
+            override fun bind(container: DayViewContainer, day: CalendarDay) {
+                container.textView.text = day.date.dayOfMonth.toString()
+            }
+        }
+
+        val currentMonth = YearMonth.now()
+        val firstMonth = currentMonth.minusMonths(10)
+        val lastMonth = currentMonth.plusMonths(10)
+        val firstDayOfWeek = WeekFields.of(Locale.getDefault()).firstDayOfWeek
+        binding.calendarView.setup(firstMonth, lastMonth, firstDayOfWeek)
+        binding.calendarView.scrollToMonth(currentMonth)
+
+        return binding.root
+    }
+
+    companion object {
+
+    }
+}
