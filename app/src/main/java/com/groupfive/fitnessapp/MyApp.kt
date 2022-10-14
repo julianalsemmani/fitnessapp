94e3a10fae999f6be94c34aa0a8001b5751922d7
ealangst <ealangst@hiof.no>
2022-10-14 13:43:36 +0200

    Check notifications every 10 seconds in the background
    Need to make sure in the future that user is not spammed
    after dismissing notification

--- app/src/main/java/com/groupfive/fitnessapp/MyApp.kt
+++ app/src/main/java/com/groupfive/fitnessapp/MyApp.kt
@@ -4,7 +4,8 @@ import android.app.Application
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.content.Context
-import android.os.Build
+import android.os.Handler
+import android.os.Looper
 import com.groupfive.fitnessapp.calendar.repository.TestCalendarRepository
 import java.time.Duration
 import java.time.Instant
@@ -12,14 +13,29 @@ import java.time.Instant
 class MyApp: Application() {
     private val calendarRepository = TestCalendarRepository.instance()
 
+    private val notificationHandler = Handler(Looper.getMainLooper())
+    private val notificationInterval = 10000
+
+    private lateinit var trainingNotificationService: TrainingNotificationService;
+
     override fun onCreate() {
         super.onCreate()
 
+        trainingNotificationService = TrainingNotificationService(this)
+
+        // Post notifications every notification interval
+        notificationHandler.postDelayed(object : Runnable {
+            override fun run() {
+                postPendingNotifications()
+                notificationHandler.postDelayed(this, notificationInterval.toLong())
+            }
+        }, notificationInterval.toLong())
+
         // Notification Channel
         createNotificationChannel()
+    }
 
-        // TEST:Showing the notification
-        val trainingNotificationService = TrainingNotificationService(this);
+    private fun postPendingNotifications() {
         calendarRepository.getPlannedWorkoutSessions().forEachIndexed { index, plannedWorkoutSession ->
             run {
                 val durationUntil = Duration.between(Instant.now(), plannedWorkoutSession.startTime)
