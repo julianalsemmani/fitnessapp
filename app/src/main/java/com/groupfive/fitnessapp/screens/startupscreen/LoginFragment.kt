297843dd5a443276b013296efe0a412da08c33c7
Julian Alsemmani <juliankonge@hotmail.com>
2022-10-21 13:04:57 +0200

    Added Login Activity

---
+++ app/src/main/java/com/groupfive/fitnessapp/screens/startupscreen/LoginFragment.kt
@@ -0,0 +1,11 @@
+package com.groupfive.fitnessapp.screens.startupscreen
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.groupfive.fitnessapp.R
+
+class LoginFragment : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
