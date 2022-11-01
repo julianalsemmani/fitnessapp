3c9ccb8eeed6429c469613fe35a658b5748efdce
ealangst <ealangst@hiof.no>
2022-11-01 20:22:22 +0100

    Added unfinished fragment for selecting workout to add to calendar
    However, I have a different idea for how to plan workouts. So will postpone for now

---
+++ app/src/main/java/com/groupfive/fitnessapp/screens/addworkout/WorkoutTypeViewAdapter.kt
@@ -0,0 +1,43 @@
+package com.groupfive.fitnessapp.screens.addworkout
+
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.ListAdapter
+import com.groupfive.fitnessapp.databinding.LayoutWorkoutTypeBinding
+import com.groupfive.fitnessapp.exercise.WorkoutType
+
+class WorkoutTypeViewAdapter : ListAdapter {
+//    companion object {
+////        fun createWorkoutTypeView(workoutType: WorkoutType): View {
+////            return when(workoutType) {
+////                WorkoutType.SQUAT ->
+////                WorkoutType.PUSH_UP -> TODO()
+////                WorkoutType.SIT_UP -> TODO()
+////            }
+////        }
+//    }
+//
+//    private val workoutTypes = WorkoutType.values()
+//
+//    init {
+//    }
+//
+//    override fun getCount(): Int {
+//        return workoutTypes.size
+//    }
+//
+//    override fun getItem(position: Int): Any {
+//        TODO("Not yet implemented")
+//    }
+//
+//    override fun getItemId(position: Int): Long {
+//        TODO("Not yet implemented")
+//    }
+//
+//    override fun getView(position: Int, convertView: View?, container: ViewGroup?): View {
+//        if(convertView == null) {
+//            convertView =
+//        }
+//    }
+}
