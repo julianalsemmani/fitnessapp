8791f6aab57145a38f421895fc356627daf49112
Ibragim Yusupov <70488743+IbragimYusupov@users.noreply.github.com>
2022-11-02 15:44:22 +0100

    Revert "Merge branch 'develop' into 40-design-profile-fragment"
    This reverts commit 3cc0922108480664ac0f8e95bc848a3c9323733c, reversing
    changes made to 7acdda148c1a720750861ece2a7f93af5b62bc2a.

--- app/src/main/java/com/groupfive/fitnessapp/MyApp.kt
+++ app/src/main/java/com/groupfive/fitnessapp/MyApp.kt
@@ -1,16 +1,63 @@
 package com.groupfive.fitnessapp
 
 import android.app.Application
-import android.content.Intent
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Handler
+import android.os.Looper
+import com.groupfive.fitnessapp.calendar.repository.TestCalendarRepository
+import java.time.Duration
+import java.time.Instant
 
 class MyApp: Application() {
+    private val calendarRepository = TestCalendarRepository.instance()
+
+    private val notificationHandler = Handler(Looper.getMainLooper())
+    private val notificationInterval = 10000
+
+    private lateinit var trainingNotificationService: TrainingNotificationService;
 
     override fun onCreate() {
         super.onCreate()
 
-        Intent(this, TrainingNotificationService::class.java).also { intent ->
-            startService(intent)
-        }
+        trainingNotificationService = TrainingNotificationService(this)
 
+        // Post notifications every notification interval
+        notificationHandler.postDelayed(object : Runnable {
+            override fun run() {
+                postPendingNotifications()
+                notificationHandler.postDelayed(this, notificationInterval.toLong())
+            }
+        }, notificationInterval.toLong())
+
+        // Notification Channel
+        createNotificationChannel()
+    }
+
+    private fun postPendingNotifications() {
+        calendarRepository.getPlannedWorkoutSessions().forEachIndexed { index, plannedWorkoutSession ->
+            run {
+                val durationUntil = Duration.between(Instant.now(), plannedWorkoutSession.startTime)
+                if(durationUntil.toMinutes() < 30) {
+                    trainingNotificationService.showNotification(index,
+                        durationUntil.toMinutes().toInt(),
+                        plannedWorkoutSession.workoutType
+                    )
+                }
+            }
+        }
+    }
+
+    private fun createNotificationChannel() {
+        val channel = NotificationChannel(
+            TrainingNotificationService.TRAINING_CHANNEL_ID,
+            "Training Reminder Channel",
+            NotificationManager.IMPORTANCE_HIGH
+        )
+        channel.description = "Used for training reminder notification."
+
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.createNotificationChannel(channel)
     }
 }
--- app/src/main/java/com/groupfive/fitnessapp/calendar/repository/PlannedWorkoutSession.kt
+++ app/src/main/java/com/groupfive/fitnessapp/calendar/repository/PlannedWorkoutSession.kt
@@ -3,4 +3,4 @@ package com.groupfive.fitnessapp.calendar.repository
 import com.groupfive.fitnessapp.exercise.WorkoutType
 import java.time.Instant
 
-data class PlannedWorkoutSession(val id: String, val startTime: Instant, val endTime: Instant, val workoutType: WorkoutType)
+data class PlannedWorkoutSession(val startTime: Instant, val endTime: Instant, val workoutType: WorkoutType)
