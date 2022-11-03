95cfcbebc3bc85b323cc41a46ab6325b0f90a974
ealangst <ealangst@hiof.no>
2022-11-03 21:21:39 +0100

    Button to complete exercise that submits workout session

---
+++ app/src/main/java/com/groupfive/fitnessapp/screens/exercisecamera/ExerciseCameraViewModel.kt
@@ -0,0 +1,53 @@
+package com.groupfive.fitnessapp.screens.exercisecamera
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.groupfive.fitnessapp.exercise.WorkoutType
+import com.groupfive.fitnessapp.model.calendar.repository.FirebaseCalendarRepository
+import com.groupfive.fitnessapp.model.workout.repository.FirebaseWorkoutSessionRepository
+import kotlinx.coroutines.runBlocking
+import java.time.Duration
+import java.time.Instant
+import java.time.LocalDate
+
+class ExerciseCameraViewModel: ViewModel() {
+    private val workoutSessionRepository = FirebaseWorkoutSessionRepository()
+
+    private val startTime = Instant.now()
+
+    private val _workoutType = MutableLiveData<WorkoutType>()
+    val workoutType: LiveData<WorkoutType>
+        get() = _workoutType
+
+    private val _reps = MutableLiveData<Int>(0)
+    val reps: LiveData<Int>
+        get() = _reps
+
+    fun setWorkoutType(workoutType: WorkoutType) {
+        _workoutType.value = workoutType
+    }
+
+    fun addRep() {
+        _reps.value = _reps.value!! + 1
+    }
+
+    fun submitWorkoutSession() {
+        if(_reps.value!! > 0 && workoutType.value != null) {
+            runBlocking {
+                workoutSessionRepository.createWorkoutSession(
+                    startTime = startTime,
+                    endTime = Instant.now()!!,
+                    reps = reps.value!!,
+                    workoutType = workoutType.value!!
+                )
+
+                _reps.value = 0
+            }
+        } else {
+            Log.e(javaClass.simpleName, "Failed to submit workout session, invalid reps or workout type")
+        }
+    }
+
+}
