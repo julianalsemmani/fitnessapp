ab7be0d733490c261d68290102ae94967515c2dd
ealangst <ealangst@hiof.no>
2022-11-04 04:16:33 +0100

    Add constraints for legs in SitUpExerciseDetector

--- app/src/main/java/com/groupfive/fitnessapp/exercise/SitUpExerciseDetector.kt
+++ app/src/main/java/com/groupfive/fitnessapp/exercise/SitUpExerciseDetector.kt
@@ -11,14 +11,24 @@ class SitUpExerciseDetector : ExerciseDetector {
         ExerciseAngleConstraint(PoseLandmark.LEFT_KNEE, PoseLandmark.LEFT_HIP, PoseLandmark.LEFT_SHOULDER,
             ComparisonType.SMALLER_THAN, 80.0, safetyMargin),
         ExerciseAngleConstraint(PoseLandmark.RIGHT_KNEE, PoseLandmark.RIGHT_HIP, PoseLandmark.RIGHT_SHOULDER,
-            ComparisonType.SMALLER_THAN, 80.0, safetyMargin)
-    )
+            ComparisonType.SMALLER_THAN, 80.0, safetyMargin),
+
+        ExerciseAngleConstraint(PoseLandmark.LEFT_ANKLE, PoseLandmark.LEFT_KNEE, PoseLandmark.LEFT_HIP,
+            ComparisonType.EQUAL, 90.0, safetyMargin),
+        ExerciseAngleConstraint(PoseLandmark.RIGHT_ANKLE, PoseLandmark.RIGHT_KNEE, PoseLandmark.RIGHT_HIP,
+            ComparisonType.EQUAL, 90.0, safetyMargin)
+        )
 
     private val sitDownConstraints = ExercisePoseConstraints(
         ExerciseAngleConstraint(PoseLandmark.LEFT_KNEE, PoseLandmark.LEFT_HIP, PoseLandmark.LEFT_SHOULDER,
             ComparisonType.GREATER_THAN, 120.0, safetyMargin),
         ExerciseAngleConstraint(PoseLandmark.RIGHT_KNEE, PoseLandmark.RIGHT_HIP, PoseLandmark.RIGHT_SHOULDER,
-            ComparisonType.GREATER_THAN, 120.0, safetyMargin)
+            ComparisonType.GREATER_THAN, 120.0, safetyMargin),
+
+        ExerciseAngleConstraint(PoseLandmark.LEFT_ANKLE, PoseLandmark.LEFT_KNEE, PoseLandmark.LEFT_HIP,
+            ComparisonType.EQUAL, 90.0, safetyMargin),
+        ExerciseAngleConstraint(PoseLandmark.RIGHT_ANKLE, PoseLandmark.RIGHT_KNEE, PoseLandmark.RIGHT_HIP,
+            ComparisonType.EQUAL, 90.0, safetyMargin)
     )
 
     private var currentConstraints = sitUpConstraints
