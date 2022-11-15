e0d440072506303d3e65f5e063aebcac412620bb
ealangst <ealangst@hiof.no>
2022-11-16 00:51:59 +0100

    Use 3D angle calculation instead of 2D
    Appears to improve calculations, especially when user is not facing camera

--- app/src/main/java/com/groupfive/fitnessapp/exercise/ExerciseUtils.kt
+++ app/src/main/java/com/groupfive/fitnessapp/exercise/ExerciseUtils.kt
@@ -2,18 +2,27 @@ package com.groupfive.fitnessapp.exercise
 
 import com.google.mlkit.vision.pose.Pose
 import com.google.mlkit.vision.pose.PoseLandmark
-import kotlin.math.abs
-import kotlin.math.atan2
+import kotlin.math.*
 
 class ExerciseUtils {
     companion object {
         fun getAngle(firstPoint: PoseLandmark, midPoint: PoseLandmark, lastPoint: PoseLandmark): Double {
-            var result = Math.toDegrees(
-                (atan2(lastPoint.position.y - midPoint.position.y,
-                    lastPoint.position.x - midPoint.position.x)
-                        - atan2(firstPoint.position.y - midPoint.position.y,
-                    firstPoint.position.x - midPoint.position.x)).toDouble()
+            val a = listOf(
+                firstPoint.position3D.x - midPoint.position3D.x,
+                firstPoint.position3D.y - midPoint.position3D.y,
+                firstPoint.position3D.z - midPoint.position3D.z
             )
+            val b = listOf(
+                lastPoint.position3D.x - midPoint.position3D.x,
+                lastPoint.position3D.y - midPoint.position3D.y,
+                lastPoint.position3D.z - midPoint.position3D.z
+            )
+            val dot = a[0] * b[0] + a[1] * b[1] + a[2] * b[2]
+            val aLength = sqrt((a[0].pow(2) + a[1].pow(2) + a[2].pow(2)).toDouble())
+            val bLength = sqrt((b[0].pow(2) + b[1].pow(2) + b[2].pow(2)).toDouble())
+            val cos = dot / (aLength * bLength)
+
+            var result = Math.toDegrees(acos(cos))
             result = abs(result) // Angle should never be negative
             if (result > 180) {
                 result = 360.0 - result // Always get the acute representation of the angle
@@ -21,7 +30,7 @@ class ExerciseUtils {
             return result
         }
 
-        fun getAngle(pose: Pose, firstPoint: Int, midPoint: Int, lastPoint: Int) : Double {
+        fun getAngle(pose: Pose, firstPoint: Int, midPoint: Int, lastPoint: Int): Double {
             return getAngle(
                 pose.getPoseLandmark(firstPoint)!!,
                 pose.getPoseLandmark(midPoint)!!,
