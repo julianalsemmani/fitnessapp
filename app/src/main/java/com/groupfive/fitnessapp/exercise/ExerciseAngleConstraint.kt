73004b197e1010be230e508078dc222a5db51c22
ealangst <ealangst@hiof.no>
2022-11-23 03:28:24 +0100

    Improved squat angles and fixed pass rate calculation

--- app/src/main/java/com/groupfive/fitnessapp/exercise/ExerciseAngleConstraint.kt
+++ app/src/main/java/com/groupfive/fitnessapp/exercise/ExerciseAngleConstraint.kt
@@ -1,5 +1,6 @@
 package com.groupfive.fitnessapp.exercise
 
+import android.util.Log
 import com.google.mlkit.vision.pose.Pose
 import com.groupfive.fitnessapp.util.PoseUtils
 import kotlin.math.abs
@@ -13,24 +14,28 @@ class ExerciseAngleConstraint(
     private val margin: Double) : ExercisePoseConstraint {
 
     override fun evaluate(pose: Pose): ExercisePoseConstraint.ConstraintResult {
-        val currentAngle = PoseUtils.getAngle(pose, firstPoint, midPoint, lastPoint)
+        val currentAngle = PoseUtils.getAngle2D(pose, firstPoint, midPoint, lastPoint)
 
-        return when (comparisonType) {
+        val result = when (comparisonType) {
             ComparisonType.GREATER_THAN -> ExercisePoseConstraint.ConstraintResult(
                 currentAngle > angle - margin,
-                (angle - currentAngle) / angle
+                currentAngle / angle
             )
             ComparisonType.EQUAL -> ExercisePoseConstraint.ConstraintResult(
                 currentAngle > angle - margin/2 && currentAngle < angle + margin/2,
-                if(currentAngle < angle) 
-                    abs(currentAngle - angle) / angle
+                if(currentAngle < angle)
+                    currentAngle / angle
                 else
-                    1 - abs(currentAngle - angle) / (180.0 - angle)
+                    1.0 - abs(currentAngle - angle) / (180.0 - angle)
             )
             ComparisonType.SMALLER_THAN -> ExercisePoseConstraint.ConstraintResult(
                 currentAngle < angle + margin,
                 1.0 - (currentAngle - angle) / (180.0 - angle)
             )
         }
+
+//        Log.e(javaClass.simpleName, angle.toString() + " - " + currentAngle.toString() + " - " + result.passRate.toString())
+
+        return result
     }
 }
