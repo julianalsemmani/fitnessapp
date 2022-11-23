904fac0249a48b450b3c3d7526bace395e5ec56b
ealangst <ealangst@hiof.no>
2022-11-23 20:41:48 +0100

    Restore HomeFragment code in ExerciseFragment

--- app/src/main/java/com/groupfive/fitnessapp/screens/exercise/ExerciseFragment.kt
+++ app/src/main/java/com/groupfive/fitnessapp/screens/exercise/ExerciseFragment.kt
@@ -5,7 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.groupfive.fitnessapp.databinding.FragmentExerciseBinding
+import com.groupfive.fitnessapp.screens.selectworkout.WorkoutTypeAdapter
 
 class ExerciseFragment : Fragment() {
     private lateinit var binding: FragmentExerciseBinding
@@ -15,6 +18,13 @@ class ExerciseFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentExerciseBinding.inflate(inflater)
+
+        binding.workoutList.layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
+        binding.workoutList.adapter = WorkoutTypeAdapter() { selectedWorkoutType ->
+            findNavController().navigate(
+                ExerciseFragmentDirections.actionHomeFragmentToExerciseCameraFragment(selectedWorkoutType)
+            )
+        }
 
         return binding.root
     }
