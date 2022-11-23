3bc22756668a57d736e166a83e70258abad80fe6
ealangst <ealangst@hiof.no>
2022-11-23 19:56:12 +0100

    Improved push up angles

--- app/src/main/java/com/groupfive/fitnessapp/exercise/PushUpExerciseDetector.kt
+++ app/src/main/java/com/groupfive/fitnessapp/exercise/PushUpExerciseDetector.kt
@@ -5,20 +5,30 @@ import com.google.mlkit.vision.pose.PoseLandmark
 
 class PushUpExerciseDetector : ExerciseDetector {
 
-    private val safetyMargin = 10.0
+    private val safetyMargin = 15.0
 
     private val pushUpConstraints = ExercisePoseConstraints(
-        ExerciseAngleConstraint(PoseLandmark.LEFT_KNEE, PoseLandmark.LEFT_SHOULDER, PoseLandmark.LEFT_WRIST,
-            ComparisonType.SMALLER_THAN, 10.0, safetyMargin),
-        ExerciseAngleConstraint(PoseLandmark.RIGHT_KNEE, PoseLandmark.RIGHT_SHOULDER, PoseLandmark.RIGHT_WRIST,
-            ComparisonType.SMALLER_THAN, 10.0, safetyMargin)
+        ExerciseAngleConstraint(PoseLandmark.LEFT_KNEE, PoseLandmark.LEFT_EAR, PoseLandmark.LEFT_WRIST,
+            ComparisonType.GREATER_THAN, 40.0, safetyMargin),
+        ExerciseAngleConstraint(PoseLandmark.RIGHT_KNEE, PoseLandmark.RIGHT_EAR, PoseLandmark.RIGHT_WRIST,
+            ComparisonType.GREATER_THAN, 40.0, safetyMargin),
+
+        ExerciseAngleConstraint(PoseLandmark.LEFT_WRIST, PoseLandmark.LEFT_ELBOW, PoseLandmark.LEFT_SHOULDER,
+            ComparisonType.GREATER_THAN, 160.0, safetyMargin),
+        ExerciseAngleConstraint(PoseLandmark.RIGHT_WRIST, PoseLandmark.RIGHT_ELBOW, PoseLandmark.RIGHT_SHOULDER,
+            ComparisonType.GREATER_THAN, 160.0, safetyMargin)
         )
 
     private val pushDownConstraints = ExercisePoseConstraints(
-        ExerciseAngleConstraint(PoseLandmark.LEFT_KNEE, PoseLandmark.LEFT_SHOULDER, PoseLandmark.LEFT_WRIST,
-            ComparisonType.GREATER_THAN, 20.0, safetyMargin),
-        ExerciseAngleConstraint(PoseLandmark.RIGHT_KNEE, PoseLandmark.RIGHT_SHOULDER, PoseLandmark.RIGHT_WRIST,
-            ComparisonType.GREATER_THAN, 20.0, safetyMargin)
+        ExerciseAngleConstraint(PoseLandmark.LEFT_KNEE, PoseLandmark.LEFT_EAR, PoseLandmark.LEFT_WRIST,
+            ComparisonType.SMALLER_THAN, 30.0, safetyMargin),
+        ExerciseAngleConstraint(PoseLandmark.RIGHT_KNEE, PoseLandmark.RIGHT_EAR, PoseLandmark.RIGHT_WRIST,
+            ComparisonType.SMALLER_THAN, 30.0, safetyMargin),
+
+        ExerciseAngleConstraint(PoseLandmark.LEFT_WRIST, PoseLandmark.LEFT_ELBOW, PoseLandmark.LEFT_SHOULDER,
+            ComparisonType.SMALLER_THAN, 60.0, safetyMargin),
+        ExerciseAngleConstraint(PoseLandmark.RIGHT_WRIST, PoseLandmark.RIGHT_ELBOW, PoseLandmark.RIGHT_SHOULDER,
+            ComparisonType.SMALLER_THAN, 60.0, safetyMargin)
     )
 
     private var currentConstraints = pushUpConstraints
